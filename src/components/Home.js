e4dc35841099bf7e8408d15b0f9b1cd46f81fd3d
Aijaz <aijazkhan405@gmail.com>
2021-10-21 21:20:33 +1300

    Updated Home Component Message

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ class Home extends Component {
     render() {
         return (
             <div>
-                <h1>Home</h1>
+                <h1>Home Updated</h1>
             </div>
         )
     }
